21d024e03c2985fe758c74f540cb2b798b9d1426
jose-maria-portela <josporhue@alum.us.es>
2023-05-13 17:20:19 +0200

    Methods put and delete, for the projects.

--- src/main/java/aiss/gitminer/controller/ProjectController.java
+++ src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -41,5 +41,28 @@ public class ProjectController {
         return projectRepository.save(new Project(project.getId(), project.getName(), project.getWebUrl(),
                 project.getCommits(),project.getIssues()));
     }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Project putProject(@PathVariable String id, @RequestBody @Valid Project infoToChange) throws ProjectNotFoundException{
+        Optional<Project> project = projectRepository.findById(id);
+
+        Project updateProject = project.get();
+
+        updateProject.setName(infoToChange.getName());
+        updateProject.setWebUrl(infoToChange.getWebUrl());
+        updateProject.setCommits(infoToChange.getCommits());
+        updateProject.setIssues(infoToChange.getIssues());
+
+        projectRepository.save(updateProject);
+        return updateProject;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProject(@PathVariable String id){
+        if(projectRepository.existsById(id))
+            projectRepository.deleteById(id);
+    }
 }
 
